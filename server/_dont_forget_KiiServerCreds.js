7c670b6c17b0bba8c0b0a847b380ca4b67d40f9e
ChrisLHall <c3doggy@gmail.com>
2018-11-13 09:53:37 -0800

    Fixed how KiiServerCreds is stored

---
+++ server/_dont_forget_KiiServerCreds.js
@@ -0,0 +1,18 @@
+// Change this filename to KiiServerCreds.js, but do not commit it
+
+// TODO MODIFY THESE CREDENTIALS TO MATCH THE ADMIN KII USER
+// In the Kii dashboard, you must manually add a user and ensure that user is the
+// only one with write privileges in Planets and PlayerInfo
+
+// Don't commit them to the repo, obviously
+var KiiServerCreds = function () {
+  var username = "" // username here
+  var password = "" // password here
+
+  return {
+    username: username,
+    password: password
+  }
+}
+
+module.exports = KiiServerCreds
